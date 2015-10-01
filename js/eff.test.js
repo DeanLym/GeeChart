54da897e5fab326a9d7f549690d28e6b50882418
zxzhaixiang <xiang.zhai@QRI-LAPTOP208.QRI.local>
2015-10-01 16:25:47 -0500

    add mouse over event
    bubble enlarged when mouse over. Bubble rotates when mouse over after
    stop moving.

--- js/eff.test.js
+++ js/eff.test.js
@@ -1,12 +1,21 @@
 // Our Javascript will go here.
+var selectable_obj = [];
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var canvas = document.getElementById("bubble_canvas");
-var renderer = new THREE.WebGLRenderer({ canvas: canvas });
+var renderer = new THREE.WebGLRenderer({
+  antialia: true,
+  canvas: canvas,
+  preserveDrawingBuffer: true} );
 // var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor( 0x222222 );
 document.body.appendChild( renderer.domElement );
+
+raycaster = new THREE.Raycaster();
+
+renderer.domElement.addEventListener('mousemove', onMouseMove, false);
+
 var box = new THREE.BoxGeometry( 1, 1, 1 );
 var basic_material = new THREE.MeshBasicMaterial( { color: 0xcc8822,wireframe: true } );
 var cube = new THREE.Mesh( box, basic_material );
@@ -20,8 +29,8 @@ var line_material2 = new THREE.LineBasicMaterial({
 
 var line_geometry1 = new THREE.Geometry();
 line_geometry1.vertices.push(
-new THREE.Vector3( -5, -5, 0 ),
-new THREE.Vector3( -5, 5, 0 )
+  new THREE.Vector3( -5, -5, 0 ),
+  new THREE.Vector3( -5, 5, 0 )
 );
 var line1 = new THREE.Line( line_geometry1, line_material );
 
@@ -29,16 +38,16 @@ var line1 = new THREE.Line( line_geometry1, line_material );
 
 var line_geometry2 = new THREE.Geometry();
 line_geometry2.vertices.push(
-new THREE.Vector3( -5, -5, 0.001 ),
-new THREE.Vector3( 5, -5, 0.001 )
+  new THREE.Vector3( -5, -5, 0.001 ),
+  new THREE.Vector3( 5, -5, 0.001 )
 );
 var line2 = new THREE.Line( line_geometry2, line_material );
 
 
 var line_geometry3 = new THREE.Geometry();
 line_geometry3.vertices.push(
-new THREE.Vector3( -5, -5, 0.001 ),
-new THREE.Vector3( 5, 5, 0.001 )
+  new THREE.Vector3( -5, -5, 0.001 ),
+  new THREE.Vector3( 5, 5, 0.001 )
 );
 var line3 = new THREE.Line( line_geometry3, line_material2 );
 
@@ -56,21 +65,26 @@ var segments = 64;
 var tstep = 0;
 
 for(var i = 0;i<num_circles;i++){
-radius.push(Math.random()*0.2+0.1);
-dx.push(Math.random()*0.01+0.001);
-slopes.push(Math.random()*2+3);
-decay.push(Math.random()/100+0.99);
-circles.push(CreateCircle(radius[i],segments));
-circles[i].position.x = -5;
-circles[i].position.y = -5;
-scene.add( circles[i] );
+  radius.push(Math.random()*0.2+0.1);
+  dx.push(Math.random()*0.01+0.001);
+  slopes.push(Math.random()*2+3);
+  decay.push(Math.random()/100+0.99);
+  circles.push(CreateCircle(radius[i],segments));
+  circles[i].position.x = -5;
+  circles[i].position.y = -5;
+  circles[i].name = 'bubble ' + i;
+  circles[i].rotationspeed = 0;
+  circles[i].radius = radius[i];
+  scene.add( circles[i] );
+  selectable_obj.push(circles[i]);
 }
 scene.add(line3);
+
 function CreateCircle(radius,segments){
-var circle_material = new THREE.MeshPhongMaterial( { color: 0x00cc22,wireframe: false } );
-var circleGeometry = new THREE.CircleGeometry( radius, segments );
-var circle = new THREE.Mesh( circleGeometry, circle_material );
-return circle;
+  var circle_material = new THREE.MeshPhongMaterial( { color: 0x00cc22,wireframe: false,transparent: true, opacity: 0.4, side: THREE.DoubleSide } );
+  var circleGeometry = new THREE.CircleGeometry( radius, segments );
+  var circle = new THREE.Mesh( circleGeometry, circle_material );
+  return circle;
 }
 
 var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
@@ -94,24 +108,24 @@ scene.add( directionalLight );
 //
 camera.position.z = 7;
 function render() {
-tstep ++;
-if (tstep < 1000){
-  for(var i = 0;i<num_circles;i++){
-    slopes[i] *= decay[i];
-    var flag = circles[i].position.x<5 & circles[i].position.y<5;
-    var bad = circles[i].position.x > circles[i].position.y;
-    circles[i].position.x += flag?dx[i]:0;
-    circles[i].position.y += flag?dx[i]*slopes[i]:0;
-    if(bad){
-      circles[i].material.color.setRGB (1, 0, 0);
+  tstep ++;
+  if (tstep < 1000){
+    for(var i = 0;i<num_circles;i++){
+      slopes[i] *= decay[i];
+      var flag = circles[i].position.x<5 & circles[i].position.y<5;
+      var bad = circles[i].position.x > circles[i].position.y;
+      circles[i].position.x += flag?dx[i]:0;
+      circles[i].position.y += flag?dx[i]*slopes[i]:0;
+      if(bad){
+        circles[i].material.color.setRGB (1, 0, 0);
+      }
+    }
+  }else{
+    for(var i = 0;i<num_circles;i++){
+      circles[i].rotation.x+=circles[i].rotationspeed;
+      circles[i].rotation.y+=circles[i].rotationspeed;
     }
   }
-}else{
-    for(var i = 0;i<num_circles;i++){
-      circles[i].rotation.x+=0.01;
-      circles[i].rotation.y+=0.01;
-    }
-}
 
 
   requestAnimationFrame( render );
